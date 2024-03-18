25c387b087ef19dbae3246f74bd7aa043850e7d1
Calista.E <159185314+jinyuzhe-calista-e@users.noreply.github.com>
2024-03-18 18:40:22 +0000

    done

--- index.php
+++ index.php
@@ -1,9 +1,40 @@
 <!DOCTYPE html>
-  <html>
+<html>
   <head>
-    <title>Title</title>
+    <meta charset="utf-8" />
+    <meta name="description" content="The math program, in PHP" />
+    <meta name="keywords" content="mths, ics2o" />
+    <meta name="author" content="Calista.E" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.pink-purple.min.css" />
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+    <link rel="manifest" href="site.webmanifest" />
+    <title>The math program,in PHP</title>
   </head>
   <body>
-    <?php echo '<p>Hello, World!, in PHP</p>'; ?>
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">The math program, in PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/area_and_perimeter.png" alt="laptop image" />
+      </div>
+      <div class="page-content">
+        <p>demensions of a rectangle are:5cm and 3cm.</p>
+        <p>The area is: 15 cm²</p>
+      </div>
+      <div id="perimeter-math">
+        <p>The perimeter is: 16 cm</p>
+      </div>
+      <br />
+    </main>
+    </div>
   </body>
 </html>
